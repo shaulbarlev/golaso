5dd2d76ecf8427224f4bda4930a0a1d524325fd2
Shaul Bar-Lev <shaulbarlev1@gmail.com>
2025-03-27 21:28:51 +0200

    added meta tags

--- golaso/src/app/page.tsx
+++ golaso/src/app/page.tsx
@@ -11,9 +11,26 @@ const Home = () => {
   return (
     <>
       <Head>
-        <title>Landing Page</title>
-        <meta name="description" content="גולאסו ערכים וספורט" />
+        <title>גולאסו ערכים וספורט</title>
+        <meta
+          name="description"
+          content="גולאסו - חוויה ספורטיבית בלתי נשכחת"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <main className="bg-white text-gray-800">
